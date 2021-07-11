d9b4a01cfe5b02d3e62ec083c2f831a640241f25
amalrnambiar <amalrnambiarlive@gmail.com>
2021-07-11 16:05:10 +0530

    creds removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -27,24 +29,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
   config.active_job.queue_adapter = :sidekiq
-  
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'spericorn.test@gmail.com',
-    password:             'spericorntest@1',
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
 
-  
-  config.action_mailer.default_url_options = { host: '192.168.1.154', port: 3000 }
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
